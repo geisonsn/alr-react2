66cf17d348f8b27579b94ac92b4fb815afe66eed
Geison Santos <geison.sn@gmail.com>
2019-01-21 18:51:32 -0400

    Adiciona indicativo de quem curtiu a foto

--- instalura/src/componentes/Foto.js
+++ instalura/src/componentes/Foto.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
+import Pubsub from 'pubsub-js';
 
 class FotoAtualizacoes extends Component {
 
@@ -18,8 +19,9 @@ class FotoAtualizacoes extends Component {
           throw new Error('Não foi possível realizar like na foto');
         }
       })
-      .then(like => {
-        this.setState({likeada : !this.state.likeada})
+      .then(liker => {
+        this.setState({likeada : !this.state.likeada});
+        Pubsub.publish('atualiza-liker', {fotoId: this.props.foto.id, liker});
       });
   }
 
@@ -38,12 +40,30 @@ class FotoAtualizacoes extends Component {
 }
 
 class FotoInfo extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {likers: this.props.foto.likers};
+    }
+    componentWillMount() {
+      Pubsub.subscribe('atualiza-liker',(topico, infoLiker) => {
+        if(this.props.foto.id === infoLiker.fotoId){
+          const possivelLiker = this.state.likers.find(liker => liker.login === infoLiker.liker.login);
+          if(possivelLiker === undefined){
+            const novosLikers = this.state.likers.concat(infoLiker.liker);
+            this.setState({likers:novosLikers});
+          } else {
+            const novosLikers = this.state.likers.filter(liker => liker.login !== infoLiker.liker.login);
+            this.setState({likers:novosLikers});
+          }  
+        }
+      });
+    }
     render(){
         return (
             <div className="foto-info">
               <div className="foto-info-likes">
                 {
-                  this.props.foto.likers.map(liker => {
+                  this.state.likers.map(liker => {
                     return (<Link key={liker.login} to={`/timeline/${liker.login}`}>{liker.login},</Link>)
                   })
                 }
