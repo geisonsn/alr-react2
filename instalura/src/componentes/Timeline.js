9e884ffcd97ae80b605b9602255faf12800c48c6
Geison Santos <geison.sn@gmail.com>
2019-01-23 16:22:36 -0400

    Converte componente de fotos em Presentational Component, responsável por só apresentar dados

--- instalura/src/componentes/Timeline.js
+++ instalura/src/componentes/Timeline.js
@@ -68,7 +68,7 @@ export default class Timeline extends Component {
       });
       Pubsub.subscribe('atualiza-liker',(topico, infoLiker) => {
         const fotoAchada = this.state.fotos.find(foto => foto.id === infoLiker.fotoId);
-        
+        fotoAchada.likeada =!fotoAchada.likeada;
         const possivelLiker = fotoAchada.likers.find(liker => liker.login === infoLiker.liker.login);
         if(possivelLiker === undefined){
           fotoAchada.likers.push(infoLiker.liker);
