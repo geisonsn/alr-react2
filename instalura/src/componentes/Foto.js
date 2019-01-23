848590322ac3851a041e564e5d192e820dc114f5
Geison Santos <geison.sn@gmail.com>
2019-01-23 15:41:08 -0400

    Move regras de negócio do componente FotoItem para o componente Timeline

--- instalura/src/componentes/Foto.js
+++ instalura/src/componentes/Foto.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
-import Pubsub from 'pubsub-js';
 
 class FotoAtualizacoes extends Component {
 
@@ -35,40 +34,12 @@ class FotoAtualizacoes extends Component {
 }
 
 class FotoInfo extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        likers: this.props.foto.likers,
-        comentarios: this.props.foto.comentarios
-      };
-    }
-    componentWillMount() {
-      Pubsub.subscribe('atualiza-liker',(topico, infoLiker) => {
-        if(this.props.foto.id === infoLiker.fotoId){
-          const possivelLiker = this.state.likers.find(liker => liker.login === infoLiker.liker.login);
-          if(possivelLiker === undefined){
-            const novosLikers = this.state.likers.concat(infoLiker.liker);
-            this.setState({likers:novosLikers});
-          } else {
-            const novosLikers = this.state.likers.filter(liker => liker.login !== infoLiker.liker.login);
-            this.setState({likers:novosLikers});
-          }  
-        }
-      });
-
-      Pubsub.subscribe('novos-comentarios', (topico, infoComentario) => {
-        if (this.props.foto.id === infoComentario.fotoId) {
-          const novosComentarios = this.state.comentarios.concat(infoComentario.novoComentario);
-          this.setState({comentarios: novosComentarios});
-        }
-      });
-    }
     render(){
         return (
             <div className="foto-info">
               <div className="foto-info-likes">
                 {
-                  this.state.likers.map(liker => {
+                  this.props.foto.likers.map(liker => {
                     return (<Link key={liker.login} to={`/timeline/${liker.login}`}>{liker.login},</Link>)
                   })
                 }
@@ -84,7 +55,7 @@ class FotoInfo extends Component {
 
               <ul className="foto-info-comentarios">
                 {
-                  this.state.comentarios.map(comentario => {
+                  this.props.foto.comentarios.map(comentario => {
                     return (
                       <li className="comentario" key={comentario.id}>
                         <Link to={`/timeline/${comentario.login}`} className="foto-info-autor">{comentario.login} </Link>
