55dfb6f0d4ddb370720593d9bb02b8c084028bde
Geison Santos <geison.sn@gmail.com>
2019-01-23 09:44:53 -0400

    Substitui propriedades do componente FotoAtualizacoes pelo recurso de Spread Operator

--- instalura/src/componentes/Foto.js
+++ instalura/src/componentes/Foto.js
@@ -124,7 +124,7 @@ export default class FotoItem extends Component {
             <FotoHeader foto={this.props.foto}/>
             <img alt="foto" className="foto-src" src={this.props.foto.urlFoto}/>
             <FotoInfo foto={this.props.foto}/>
-            <FotoAtualizacoes foto={this.props.foto} like={this.props.like} comenta={this.props.comenta}/>
+            <FotoAtualizacoes {...this.props}/>
           </div>            
         );
     }
